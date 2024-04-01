763754450feaec6ab115cfd340833968ebeec201
djdongjae <jk47wee@gmail.com>
2024-04-01 14:01:34 +0900

    init: 프로젝트 초기 설정 (#2)

---
+++ src/main/java/net/skhu/tastyinventory_be/TastyInventoryBeApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.tastyinventory_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyInventoryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyInventoryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/skhu/tastyinventory_be/TastyInventoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu.tastyinventory_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyInventoryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
