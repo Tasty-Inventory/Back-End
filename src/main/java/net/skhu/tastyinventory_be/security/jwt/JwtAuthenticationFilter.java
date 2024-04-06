8e0ec19af1c999efcb959ee439e85bfb1c8b93bd
djdongjae <jk47wee@gmail.com>
2024-04-06 18:48:14 +0900

    feat: jwt 토큰 발급 (#4)

---
+++ src/main/java/net/skhu/tastyinventory_be/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package net.skhu.tastyinventory_be.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import net.skhu.tastyinventory_be.security.UserDetailsImpl;
+import net.skhu.tastyinventory_be.security.UserDetailsServiceImpl;
+import net.skhu.tastyinventory_be.util.CookieUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String username = null;
+        String jwt = null;
+
+        Optional<Cookie> jwtCookie = CookieUtils.getCookie(request, "access_token");
+
+        if (jwtCookie.isPresent()) {
+            jwt = jwtCookie.get().getValue();
+            username = jwtProvider.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
+
+            if (jwtProvider.validateToken(jwt, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+}
