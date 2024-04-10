d56e2c6d61cb60a29d19a404eb9034065f54cf1f
shinheekim <shinhee1241@gmail.com>
2024-04-10 09:06:54 +0900

    feat: 재고 등록(#6)

--- src/main/java/net/skhu/tastyinventory_be/TastyInventoryBeApplication.java
+++ src/main/java/net/skhu/tastyinventory_be/TastyInventoryBeApplication.java
@@ -2,7 +2,9 @@ package net.skhu.tastyinventory_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  //추가
 @SpringBootApplication
 public class TastyInventoryBeApplication {
 
--- src/main/java/net/skhu/tastyinventory_be/dto/InventoryDto.java
+++ src/main/java/net/skhu/tastyinventory_be/dto/InventoryDto.java
@@ -1,4 +1,31 @@
 package net.skhu.tastyinventory_be.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.skhu.tastyinventory_be.domain.inventory.Inventory;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InventoryDto {
+    private Long id;
+    private  String name;
+    private  String unit;
+    private String imageUrl;
+
+    @Builder
+    public InventoryDto(String name, String unit, String imageUrl) {
+        this.name = name;
+        this.unit = unit;
+        this.imageUrl = imageUrl;
+    }
+    public Inventory toEntity(){
+        return Inventory.builder()
+                .name(name)
+                .unit(unit)
+                .imageUrl(imageUrl)
+                .build();
+    }
 }
