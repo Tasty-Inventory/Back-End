a931913ee5cbf999a10cd137cb1f4e13cf2d9d2a
djdongjae <jk47wee@gmail.com>
2024-04-11 14:25:52 +0900

    test: 로그인 테스트

--- src/main/java/net/skhu/tastyinventory_be/controller/user/UserController.java
+++ src/main/java/net/skhu/tastyinventory_be/controller/user/UserController.java
@@ -6,8 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import net.skhu.tastyinventory_be.common.dto.BaseResponse;
 import net.skhu.tastyinventory_be.controller.user.dto.SignUpRequestDto;
 import net.skhu.tastyinventory_be.exception.SuccessCode;
+import net.skhu.tastyinventory_be.security.UserDetailsImpl;
 import net.skhu.tastyinventory_be.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -21,5 +23,11 @@ public class UserController {
     public BaseResponse signUpNewUser(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
         userService.saveUser(signUpRequestDto);
         return BaseResponse.success(SuccessCode.SIGNUP_SUCCESS);
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse<String> signInTest(@AuthenticationPrincipal UserDetailsImpl loginUser) {
+        return BaseResponse.success(SuccessCode.LOGIN_SUCCESS, loginUser.getEmail());
     }
 }
