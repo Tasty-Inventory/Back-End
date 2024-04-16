3da8f233c150d51078b8f48e9b0b1ac23b347f45
CINI <shinhee1241@gmail.com>
2024-04-17 01:26:35 +0900

    fix: run 에러 해결

--- src/main/java/net/skhu/tastyinventory_be/controller/inventory/InventoryController.java
+++ src/main/java/net/skhu/tastyinventory_be/controller/inventory/InventoryController.java
@@ -2,10 +2,8 @@ package net.skhu.tastyinventory_be.controller.inventory;
 
 import lombok.RequiredArgsConstructor;
 
-import net.skhu.tastyinventory_be.controller.inventory.dto.InventoryRequestDto;
 import net.skhu.tastyinventory_be.dto.InventorySaveRequestDto;
 import net.skhu.tastyinventory_be.service.Inventory.InventoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
