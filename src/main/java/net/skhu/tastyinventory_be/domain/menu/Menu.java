7347af37a307331c623d9194436dd43f8fd6df98
djdongjae <jk47wee@gmail.com>
2024-04-23 13:25:17 +0900

    feat: menu 도메인 생성 (#6)

---
+++ src/main/java/net/skhu/tastyinventory_be/domain/menu/Menu.java
@@ -0,0 +1,30 @@
+package net.skhu.tastyinventory_be.domain.menu;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.skhu.tastyinventory_be.domain.BaseEntity;
+
+@Getter
+@Table(name = "MENU")
+@NoArgsConstructor
+@Entity
+public class Menu extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    private Long price;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Builder
+    public Menu(String name, Long price, String image) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+}
