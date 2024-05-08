e5e5bdeaefa7e709393a4536f02758af0386173d
djdongjae <jk47wee@gmail.com>
2024-05-08 11:27:58 +0900

    feat: 레시피 도메인 설정 (#6)

---
+++ src/main/java/net/skhu/tastyinventory_be/domain/recipe/Recipe.java
@@ -0,0 +1,34 @@
+package net.skhu.tastyinventory_be.domain.recipe;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.skhu.tastyinventory_be.domain.BaseEntity;
+import net.skhu.tastyinventory_be.domain.inventory.Inventory;
+import net.skhu.tastyinventory_be.domain.menu.Menu;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "RECIPE")
+@Entity
+public class Recipe extends BaseEntity {
+    @Column(nullable = false)
+    private Long usage;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+
+    @Builder
+    public Recipe(Long usage, Menu menu, Inventory inventory) {
+        this.usage = usage;
+        this.menu = menu;
+        this.inventory = inventory;
+    }
+}
