30ac7a854057fc8f613dcf70167e088d9fb63ad6
Kim, yongwook <kyw4846@gmail.com>
2024-06-11 20:54:42 +0900

    feat. 스케줄 최종 (#15)

--- src/main/java/net/skhu/tastyinventory_be/domain/schedule/ScheduleRepository.java
+++ src/main/java/net/skhu/tastyinventory_be/domain/schedule/ScheduleRepository.java
@@ -1,7 +1,5 @@
 package net.skhu.tastyinventory_be.domain.schedule;
 
-
-import net.skhu.tastyinventory_be.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
