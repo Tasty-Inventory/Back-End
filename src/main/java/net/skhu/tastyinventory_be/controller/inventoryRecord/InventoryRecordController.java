a8415a48a069effbe3b140713f74a7fc9b59ba3b
CINI <shinhee1241@gmail.com>
2024-06-16 20:57:01 +0900

    feat: 재고 정보 CRUD

--- src/main/java/net/skhu/tastyinventory_be/controller/inventoryRecord/InventoryRecordController.java
+++ src/main/java/net/skhu/tastyinventory_be/controller/inventoryRecord/InventoryRecordController.java
@@ -4,11 +4,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.skhu.tastyinventory_be.common.dto.BaseResponse;
+import net.skhu.tastyinventory_be.controller.inventoryRecord.dto.request.InventoryRecordDto;
 import net.skhu.tastyinventory_be.controller.inventoryRecord.dto.request.InventoryRecordRequestDto;
+import net.skhu.tastyinventory_be.controller.inventoryRecord.dto.request.InventoryRecordUpdateRequestDto;
+import net.skhu.tastyinventory_be.controller.inventoryRecord.dto.response.InventoryRecordResponseDto;
+import net.skhu.tastyinventory_be.domain.inventoryRecord.InventoryRecord;
 import net.skhu.tastyinventory_be.exception.SuccessCode;
 import net.skhu.tastyinventory_be.service.InventoryRecordService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,4 +29,21 @@ public class InventoryRecordController {
         inventoryRecordService.createInventoryRecord(requestDto);
         return BaseResponse.success(SuccessCode.INVENTORY_RECORD_CREATE_SUCCESS);
     }
+    @GetMapping
+    public BaseResponse<List<InventoryRecordResponseDto>> getAllRecords() {
+        return BaseResponse.success(SuccessCode.INVENTORY_RECORD_GET_SUCCESS, inventoryRecordService.getAllInventoryRecords());
+    }
+    @PatchMapping("/{id}")
+    public BaseResponse<?> updateRecord(@PathVariable Long id, @RequestBody @Valid InventoryRecordUpdateRequestDto requestDto) {
+        inventoryRecordService.updateInventoryRecord(id, requestDto);
+        return BaseResponse.success(SuccessCode.INVENTORY_RECORD_PATCH_SUCCESS);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public BaseResponse<?> deleteRecord(@PathVariable Long id) {
+        inventoryRecordService.deleteInventoryRecord(id);
+        return BaseResponse.success(SuccessCode.INVENTORY_RECORD_DELETE_SUCCESS);
+    }
+
 }
